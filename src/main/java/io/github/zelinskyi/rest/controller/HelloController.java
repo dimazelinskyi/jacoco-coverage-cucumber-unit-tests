d7f30af07bfba5475fb820863dbda7cf66d53467
Dmytro Zelinskyi <Dmytro_Zelinskyi@epam.com>
2016-11-29 12:22:46 +0200

    [gherkin-and-jacoco-integration-3] - [Improve example with maven profile etc] : improved relation between classes

--- src/main/java/io/github/zelinskyi/rest/controller/HelloController.java
+++ src/main/java/io/github/zelinskyi/rest/controller/HelloController.java
@@ -1,5 +1,6 @@
 package io.github.zelinskyi.rest.controller;
 
+import io.github.zelinskyi.services.HelloService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,10 +10,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     
+    private HelloService helloService;
+    
+    public HelloController(HelloService helloService) {
+        this.helloService = helloService;
+    }
     
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     @ResponseBody
     public HttpStatus helloStatus() {
-        return HttpStatus.OK;
+        
+        return helloService.hello() == null ? HttpStatus.NOT_FOUND : HttpStatus.OK;
     }
 }
--- src/test/java/io/github/zelinskyi/rest/controller/HelloControllerTest.java
+++ src/test/java/io/github/zelinskyi/rest/controller/HelloControllerTest.java
@@ -1,18 +1,28 @@
 package io.github.zelinskyi.rest.controller;
 
 
+import io.github.zelinskyi.services.HelloService;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 import org.springframework.http.HttpStatus;
 
 import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.Assert.assertThat;
+import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class HelloControllerTest {
+    
+    @Mock
+    private HelloService helloService;
     
     @Test
     public void testHelloStatus() {
         
-        HelloController controller = new HelloController();
+        when(helloService.hello()).thenReturn("hello");
+        HelloController controller = new HelloController(helloService);
         assertThat(controller.helloStatus(), equalTo(HttpStatus.OK));
     }
 }
